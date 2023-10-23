a4e040ffd7a53c56fee40f25328f32e35d6ae079
Vinicius Borba <vlborba12@gmail.com>
2023-10-23 12:38:06 -0300

    first commit

---
+++ Usuários.py
@@ -0,0 +1,5 @@
+from Classes import Nome, Sobrenome, Responsável, Idade, Registro
+
+abc001 = Nome('Clarice'), Sobrenome('Lispector'), Idade(27), Responsável('Mania'), Registro('abc001')
+abc002 = Nome('Frida'), Sobrenome('Kahlo'), Idade(30), Responsável('Matilde'), Registro('abc002')
+abc003 = Nome('Marie'), Sobrenome('Curie'), Idade(48), Responsável('Wladyslaw'), Registro('abc003')
